0b4945f803fa49ad2207cd7faf35331e4c8b52d9
Ryan Swanstrom <git@swgoof.com>
2012-10-24 23:49:10 -0500

    sending urls to pages

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,12 @@
 package controllers;
 
-import play.mvc.*;
-
-
-import models.*;
+import java.util.HashMap;
+import java.util.Map;
+import models.Item;
+import models.Search;
 import org.apache.commons.lang.StringUtils;
-import play.Logger;
+import play.mvc.Controller;
+import play.mvc.Router;
 import utils.GoogleSearch;
 
 public class Application extends Controller {
@@ -27,13 +28,19 @@ public class Application extends Controller {
     public static void results(String id) {
         Search result = Search.findById(id);
         notFoundIfNull(result);
-        render(result);
+        Map map = new HashMap();
+        map.put("id", id);
+        String url = Router.getFullUrl("Application.results", map);
+        render(result, url);
     }
     
     public static void showItem(String id) {
         Item item = Item.findById(id);
         notFoundIfNull(item);
-        render(item);
+        Map map = new HashMap();
+        map.put("id", id);
+        String url = Router.getFullUrl("Application.showItem", map);
+        render(item, url);
     }
     
     public static void about() {
